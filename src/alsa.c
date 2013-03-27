ab926b3b8624dfebc6878d94cff7923079979526
dillya <dillya@sparod.com>
2013-03-27 17:40:32 +0100

    Add (empty) ALSA output

---
+++ src/alsa.c
@@ -0,0 +1,30 @@
+/*
+ * alsa.c - Alsa output
+ *
+ * Copyright (c) 2013   A. Dilly
+ *
+ * AirCat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * AirCat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AirCat.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <asoundlib.h>
+
+#include "alsa.h"
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
