55a2c27a54258acb9dacdcd866787e4905a7e086
dillya <dillya@sparod.com>
2014-05-15 19:18:14 +0200

    Activated experimental cache for Radio module

--- src/radio.c
+++ src/radio.c
@@ -91,7 +91,7 @@ int radio_play(struct radio_handle *h, const char *id)
 	channels = shoutcast_get_channels(h->shout);
 
 	/* Open new Audio stream output and play */
-	h->stream = output_add_stream(h->output, samplerate, channels, 0, 0,
+	h->stream = output_add_stream(h->output, samplerate, channels, 1000, 1,
 				      &shoutcast_read, h->shout);
 	output_play_stream(h->output, h->stream);
 
